1a3b9790574cece8a47ae738c4f332119fc5f939
Vegard Skjefstad <vegard@vegard.net>
2017-09-01 08:02:51 +0200

    Basic class example.

---
+++ c#/CSharpForJavaDevelopers/CSharpForJavaDevelopers/Classes/BasicStudent.cs
@@ -0,0 +1,22 @@
+﻿namespace VegardNet.CSharpForJavaDevelopers.Classes
+{
+    class BasicStudent
+    {
+        private string name;
+
+        public BasicStudent(string name)
+        {
+            this.name = name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
