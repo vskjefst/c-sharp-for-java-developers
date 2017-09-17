9494f5bc8e777b8e8b5574db63870830e2c2c2aa
Vegard Skjefstad <vegard@vegard.net>
2017-09-17 06:42:20 +0200

    Added basic pass-by-reference example.

---
+++ c#/CSharpForJavaDevelopers/CSharpForJavaDevelopers/Methods/MethodsRef.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace VegardNetCSharpForJavaDevelopers.Methods
+{
+    class MethodsRef
+    {
+        public static void MethodWithoutRef(int nonRefValue)
+        {
+            nonRefValue++;
+        }
+
+        public static void MethodWithRef(ref int refValue)
+        {
+            refValue++;
+        }
+
+        public static void Main(string[] args)
+        {
+            int nonRefValue = 5;
+            Console.WriteLine("nonRefValue before method call: " + nonRefValue);
+            MethodWithoutRef(nonRefValue);
+            Console.WriteLine("nonRefValue after method call: " + nonRefValue);
+
+            int refValue = 5;
+            Console.WriteLine("refValue before method call: " + refValue);
+            MethodWithRef(ref refValue);
+            Console.WriteLine("refValue after method call: " + refValue);
+
+            Console.ReadKey();
+        }
+    }
+}
