2d5510332bdac0756c88fce5fdf8f00d45ba5bae
Vegard Skjefstad <vegard@vegard.net>
2017-09-17 07:07:04 +0200

    Extended example with reference types to cause further confusion.

--- c#/CSharpForJavaDevelopers/CSharpForJavaDevelopers/Methods/MethodsRef.cs
+++ c#/CSharpForJavaDevelopers/CSharpForJavaDevelopers/Methods/MethodsRef.cs
@@ -4,29 +4,63 @@ namespace VegardNetCSharpForJavaDevelopers.Methods
 {
     class MethodsRef
     {
-        public static void MethodWithoutRef(int nonRefValue)
+        public static void ValueTypeMethodWithoutRef(int nonRefValue)
         {
             nonRefValue++;
+            Console.WriteLine("Value in method body: " + nonRefValue);
         }
 
-        public static void MethodWithRef(ref int refValue)
+        public static void ValueTypeMethodWithRef(ref int refValue)
         {
             refValue++;
+            Console.WriteLine("Value in method body: " + refValue);
+        }
+
+        public static void ReferenceTypeMethodWithoutRef(ReferenceType nonRefValue)
+        {
+            nonRefValue = new ReferenceType(6);
+            Console.WriteLine("Value in method body: " + nonRefValue.value);
+        }
+
+        public static void ReferenceTypeMethodWithRef(ref ReferenceType refValue)
+        {
+            refValue = new ReferenceType(6);
+            Console.WriteLine("Value in method body: " + refValue.value);
         }
 
         public static void Main(string[] args)
         {
             int nonRefValue = 5;
             Console.WriteLine("nonRefValue before method call: " + nonRefValue);
-            MethodWithoutRef(nonRefValue);
-            Console.WriteLine("nonRefValue after method call: " + nonRefValue);
+            ValueTypeMethodWithoutRef(nonRefValue);
+            Console.WriteLine("nonRefValue after method call (can't be modified, should be 5): " + nonRefValue);
 
             int refValue = 5;
-            Console.WriteLine("refValue before method call: " + refValue);
-            MethodWithRef(ref refValue);
-            Console.WriteLine("refValue after method call: " + refValue);
+            Console.WriteLine("\nrefValue before method call: " + refValue);
+            ValueTypeMethodWithRef(ref refValue);
+            Console.WriteLine("refValue after method call (can be modified, should be 6): " + refValue);
+
+            ReferenceType nonRefReferenceType = new ReferenceType(5);
+            Console.WriteLine("\nnonRefReferenceType before method call: " + nonRefReferenceType.value);
+            ReferenceTypeMethodWithoutRef(nonRefReferenceType);
+            Console.WriteLine("nonRefReferenceType after method call (can't be modified, should be 5): " + nonRefReferenceType.value);
+
+            ReferenceType refReferenceType = new ReferenceType(5);
+            Console.WriteLine("\nrefReferenceType before method call: " + refReferenceType.value);
+            ReferenceTypeMethodWithRef(ref refReferenceType);
+            Console.WriteLine("refReferenceType after method call (can be modified, should be 6): " + refReferenceType.value);
 
             Console.ReadKey();
         }
+    }
+
+    class ReferenceType
+    {
+        public ReferenceType(int value)
+        {
+            this.value = value;
+        }
+
+        public int value;
     }
 }
