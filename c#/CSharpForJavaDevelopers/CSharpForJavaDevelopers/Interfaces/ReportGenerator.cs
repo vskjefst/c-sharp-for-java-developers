3c582fbee51bae9ec9e8e60a9e8a23b2531743d3
Vegard Skjefstad <vegard@vegard.net>
2017-09-17 06:41:20 +0200

    Added static modifier so the Main method can be used to start the project.

--- c#/CSharpForJavaDevelopers/CSharpForJavaDevelopers/Interfaces/ReportGenerator.cs
+++ c#/CSharpForJavaDevelopers/CSharpForJavaDevelopers/Interfaces/ReportGenerator.cs
@@ -4,7 +4,7 @@ namespace VegardNet.CSharpForJavaDevelopers.Interfaces
 {
     class ReportGenerator
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
             new List<IReportable>() { new Student(), new Teacher() }.ForEach(r => r.GenerateReport());
         }
